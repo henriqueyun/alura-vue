da3d8838985de9d27a6e29f1422af918ebabcaf8
henriqueyun <henriqueyun@gmail.com>
2021-07-14 00:27:51 -0300

    removendo imagens da home

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1 class="centralizado">{{titulo}}</h1>
+    <p v-show="mensagem" class="centralizado">{{mensagem}}</p>
     <input type="search" v-on:input="filtro = $event.target.value" placeholder="Filtre por titulo =)" class="filtro"></input>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva v-meu-transform:scale='1.2' v-bind:url="foto.url" v-bind:titulo="titulo"/>
-          <meu-botao tipo="button" rotulo="REMOVER" :confirmacao="false" @botaoAtivado="remove(foto)" estilo=""/>
+          <meu-botao tipo="button" rotulo="REMOVER" :confirmacao="true" @botaoAtivado="remove(foto)" estilo=""/>
         </meu-painel>
       </li>
     </ul>
@@ -36,7 +37,8 @@
       return {
         titulo: 'Alurapic',
         fotos: [],
-        filtro: ''
+        filtro: '',
+        mensagem: ''
       }
     },
 
@@ -59,8 +61,16 @@
 
     methods: {
       remove(foto) {
-          alert('Imagem removida (' + foto.titulo + ')')
+        console.log(this.foto)
+        console.log(foto)
+        this.$http.delete(`http://localhost:3000/v1/fotos/${foto._id}`)
+        .then(() => this.mensagem = 'Foto removida com sucesso', (err) => {
+          console.error(err);
+          this.mensagem = 'Erro ao tentar remover a foto'
+        })
       }
+
+      
     }
   }
 </script>
